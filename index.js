4c61baaf7838a7a80886f7a5673c90f80758233e
rogeriofabricio <rogerio.fabricio@hotmail.com>
2019-05-05 10:06:52 -0300

    Init Version
    Versão inicial

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.listen(3000, err => {
+    if(err) {
+        console.log('Não foi possível rodar o servidor!')
+    } else {
+        console.log('Servidor On line!')
+    }
+})
+
